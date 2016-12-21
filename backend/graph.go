8a75a03f2f062d075f9af6b90616624ef453aec3
Christian Kniep <christian@qnib.org>
2016-12-21 14:01:31 +0100

    add GET/POST of machines

--- backend/graph.go
+++ backend/graph.go
@@ -1,9 +1,14 @@
 package backend
 
 import (
-    "sort"
-    "fmt"
-    "errors"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"sort"
+
+	"github.com/gorilla/mux"
 
 	"github.com/mkindahl/gograph/directed"
 	qinv "github.com/qnib/qinventory/lib"
@@ -25,39 +30,49 @@ func NewQGraph() (QGraph, error) {
 
 // GetNodeIDs fetches the IDs of all Nodes
 func (qg *QGraph) GetNodeIDs() ([]string, error) {
-    var ids []string
-    for k := range qg.Nodes {
-        ids = append(ids, k)
-    }
-    sort.Strings(ids)
-    return ids, nil
+	var ids []string
+	for k := range qg.Nodes {
+		ids = append(ids, k)
+	}
+	sort.Strings(ids)
+	return ids, nil
+}
+
+// GetNode fetches the IDs of all Nodes
+func (qg *QGraph) GetNode(id string) (qinv.QEntity, error) {
+	if val, ok := qg.Nodes[id]; ok {
+		e := val.GetMe()
+		return e, nil
+	}
+	var e qinv.QEntity
+	return e, fmt.Errorf("No node with id '%s'", id)
 }
 
 // IsSubset returns true if all ids provided are within the Nodes
 func (qg *QGraph) IsSubset(keys []string) bool {
-    all, _ := qg.GetNodeIDs()
-    for i := range all {
-        for _, s := range keys {
-            if s == all[i] {
-                if len(keys) == 1 {
-                    return true
-                }
-                keys = append(keys[:i], keys[i+1:]...)
-            }
-        }
-    }
-    return false
+	all, _ := qg.GetNodeIDs()
+	for i := range all {
+		for _, s := range keys {
+			if s == all[i] {
+				if len(keys) == 1 {
+					return true
+				}
+				keys = append(keys[:i], keys[i+1:]...)
+			}
+		}
+	}
+	return false
 }
 
 // IDinNodes returns true if id is part of nodes
 func (qg *QGraph) IDinNodes(id string) bool {
-    all, _ := qg.GetNodeIDs()
-    for i := range all {
-        if id == all[i] {
-            return true
-        }
-    }
-    return false
+	all, _ := qg.GetNodeIDs()
+	for i := range all {
+		if id == all[i] {
+			return true
+		}
+	}
+	return false
 }
 
 // AddNode adds a qinventory entity to the Graph
@@ -68,14 +83,78 @@ func (qg *QGraph) AddNode(e qinv.QEntity) (*QGraph, error) {
 	return qg, nil
 }
 
+// SetNode adds a qinventory entity to the Graph
+func (qg QGraph) SetNode(e qinv.QEntity) error {
+	nID := e.GetID()
+	qg.Nodes[nID] = e.GetMe()
+	qg.Graph.AddVertex(nID)
+	log.Printf("[II] Added node '%s'\n", nID)
+	return nil
+}
+
 // AddEdge connects to entities
-func (qg *QGraph) AddEdge(x,y qinv.QEntity) (*QGraph, error) {
-    ids := []string{x.GetID(),y.GetID()}
-    c := []string{x.GetID(),y.GetID()}
-    if ! qg.IsSubset(ids) {
-        msg := fmt.Sprintf("id not in graph: %s, %s", c[0], c[1])
-        return qg, errors.New(msg)
-    }
-    qg.Graph.AddEdge(x.GetID(), y.GetID())
-    return qg, nil
+func (qg *QGraph) AddEdge(x, y qinv.QEntity) (*QGraph, error) {
+	ids := []string{x.GetID(), y.GetID()}
+	c := []string{x.GetID(), y.GetID()}
+	if !qg.IsSubset(ids) {
+		msg := fmt.Sprintf("id not in graph: %s, %s", c[0], c[1])
+		return qg, errors.New(msg)
+	}
+	qg.Graph.AddEdge(x.GetID(), y.GetID())
+	return qg, nil
+}
+
+// StartHttpd creates a http serverPort
+func (qg *QGraph) StartHttpd() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", qg.Index).Methods("GET")
+	router.HandleFunc("/machines", qg.ListMachines).Methods("GET")
+	router.HandleFunc("/machine/{id}", qg.GetMachine).Methods("GET")
+	router.HandleFunc("/machine/{id}", qg.AddMachine).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+// Index shows the entry page
+func (qg *QGraph) Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+
+// ListMachines fetches all machines
+func (qg *QGraph) ListMachines(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Todo Index!")
+}
+
+// GetMachine fetches one machine
+func (qg *QGraph) GetMachine(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	m, err := qg.GetNode(id)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("No node with id '%s'", id), 500)
+	}
+	b, _ := json.Marshal(m)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
+}
+
+// AddMachine adds a machine to the graph
+func (qg *QGraph) AddMachine(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	decoder := json.NewDecoder(r.Body)
+	var m qinv.Machine
+	err := decoder.Decode(&m)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+	err = qg.SetNode(m.GetMe())
+	if err != nil {
+		panic(err)
+	}
+	m.ID = id
+	b, _ := json.Marshal(m)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
 }
--- server/server.go
+++ server/server.go
@@ -1,37 +1,16 @@
 package qserver
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/qnib/qinventory/backend"
+	qinv "github.com/qnib/qinventory/lib"
 
-	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
 )
 
 // ServeQinventory start daemon
 func ServeQinventory(cmd *cobra.Command, args []string) {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/machines", ListMachines)
-	router.HandleFunc("/machine/{id}", GetMachine)
-
-	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-// Index shows the entry page
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome!")
-}
-
-// ListMachines fetches all machines
-func ListMachines(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Todo Index!")
-}
-
-// GetMachine fetches one machine
-func GetMachine(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	ID := vars["id"]
-	fmt.Fprintln(w, "Todo show:", ID)
+	qg, _ := backend.NewQGraph()
+	m, _ := qinv.NewMachine("host1", "4.7.10")
+	qg.AddNode(m.GetMe())
+	qg.StartHttpd()
 }
